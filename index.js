c31f12ea5bc52d0457c70ec074e8918c9be5ed3f
kilcmn <kilcmn3@hotmail.com>
2020-10-08 23:08:13 -0400

    failed first itme

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-var letterCombinations = function (digits) {
-  if (digits.length === 0) return [];
+var isValid = function (s) {
   var map = {
-    2: ['a', 'b', 'c'],
-    3: ['d', 'e', 'f'],
-    4: ['g', 'h', 'i'],
-    5: ['j', 'k', 'l'],
-    6: ['m', 'n', 'o'],
-    7: ['p', 'q', 'r', 's'],
-    8: ['t', 'u', 'v'],
-    9: ['w', 'x', 'y', 'z'],
+    '{': '}',
+    '[': ']',
+    '(': ')',
   };
-  var output = digits.split('').map((target) => map[target]);
 
-  return output.reduce((acc, cum) => {
-    var container = [];
-    acc.map((a) => {
-      return (container = container.concat(cum.map((b) => a + b)));
-    });
-    return container;
-  });
+  var stack = [];
+  for (var i = 0; i < s.length; i++) {
+    var z = s[i];
+    if (map[z]) {
+      stack.push(map[z]);
+    } else if (z !== stack.pop()) {
+      return false;
+    }
+  }
+  return !stack.length;
 };
 
-console.log(letterCombinations('3'));
+console.log(isValid('()'));
