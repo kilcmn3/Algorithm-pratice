c75d49b31c25d65758c0ea32beb6060be3b0c811
kilcmn <kilcmn3@hotmail.com>
2020-10-08 23:00:20 -0400

    17 pass ok

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var compress = function(chars) {
-    let n = chars.length;
-    let count = 1;
-    
-    for (let i = n - 2; i >= 0; i--) {
-        if (chars[i] == chars[i+1]) {
-            count++;
-        } else if (count > 1) {
-            chars.splice(i+2, count-1, ...count.toString().split(''));
-     
-            count = 1;
-        }
-    }
-    
-    if (count > 1) {
-        chars.splice(1, count-1, ...count.toString().split(''));           
-    }
-    
-    return chars.length;
+var letterCombinations = function (digits) {
+  if (digits.length === 0) return [];
+  var map = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+  };
+  var output = digits.split('').map((target) => map[target]);
+
+  return output.reduce((acc, cum) => {
+    var container = [];
+    acc.map((a) => {
+      return (container = container.concat(cum.map((b) => a + b)));
+    });
+    return container;
+  });
 };
 
-var  chars = 
-["a","a","b","b","c","c","c"]
-compress(chars)
+console.log(letterCombinations('3'));
