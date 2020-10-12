89e2b038c2ee033eceefabdefaedec2be13907f7
kilcmn <kilcmn3@hotmail.com>
2020-10-12 17:06:41 -0400

    solving 1twosome

---
+++ 1twoSome.js
@@ -0,0 +1,52 @@
+/*
+https://leetcode.com/problems/two-sum/
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+*/
+
+var twoSum = function (nums, target) {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    if (hash[target - n] !== undefined) {
+      return [hash[target - n], i];
+    }
+    hash[n] = i;
+    debugger;
+  }
+  return [];
+};
+
+let nums = 6;
+let target = [3, 2, 3];
+
+twoSum(nums, target);
