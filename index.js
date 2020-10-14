5e815c715839b1a7ddb2fc27addf74a6e5802401
kilcmn <kilcmn3@hotmail.com>
2020-10-13 22:52:31 -0400

    re-pratice 1 two some

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-var twoSum = function (nums, target) {
-  let hash = {};
-
-  for (let i = 0; i < nums.length; i++) {
-    const n = nums[i];
-    if (hash[target - n] !== undefined) {
-      return [hash[target - n], i];
-    }
-    hash[n] = i;
-  }
-  console.log('just testing');
-  return [];
-};
+var twoSum = function (nums, target) {};
 
 let nums = 6;
 let target = [3, 2, 3];
