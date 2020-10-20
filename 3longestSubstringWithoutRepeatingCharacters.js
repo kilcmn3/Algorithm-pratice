f52d0c3de9e9df83a7419f264cfdac7705405f7a
kilcmn <kilcmn3@hotmail.com>
2020-10-19 22:44:51 -0400

    update 3longestSubstring...

--- 3longestSubstringWithoutRepeatingCharacters.js
+++ 3longestSubstringWithoutRepeatingCharacters.js
@@ -33,8 +33,16 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
 
-var lengthOfLongestSubstring = function(s) {
-    
-};
 
 */
+
+var lengthOfLongestSubstring = function (s) {
+  const map = {};
+  var left = 0;
+
+  return s.split('').reduce((max, v, i) => {
+    left = map[v] >= left ? map[v] + 1 : left;
+    map[v] = i;
+    return Math.max(max, i - left + 1);
+  }, 0);
+};
