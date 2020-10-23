583e0e109f06d85a29dd6284d8f100bc6e247dee
kilcmn <kilcmn3@hotmail.com>
2020-10-22 20:33:56 -0400

    added solution 5 longest..

--- 5longestPalindromicSubstring.js
+++ 5longestPalindromicSubstring.js
@@ -23,3 +23,53 @@ Example 4:
 Input: s = "ac"
 Output: "a"
 */
+
+// solution 1.
+
+var longestPalindrome = function (str) {
+  if (str.length < 1 || str === null) return '';
+
+  let longest = '';
+
+  for (let i = 0; i < str.length; i++) {
+    let oddPalindrome = expandFromCenter(str, i, i);
+    let eventPalidrome = expandFromCenter(str, i - 1, i);
+
+    if (oddPalindrome > longest.length) {
+      longest = oddPalindrome;
+    }
+    if (eventPalidrome > longest.length) {
+      longest = eventPalidrome;
+    }
+  }
+  return longest;
+};
+
+var expandFromCenter = function (str, left, rigth) {
+  let i = 0;
+  while (str[left - 1] && str[left - 1] === str[rigth + 1]) {
+    i++;
+  }
+  i--;
+  return str.slice(left - 1, right + i + 1);
+};
+
+//solution 2
+
+var longestPalindrome = function (s) {
+  var max = '';
+  for (var i = 0; i < s.length; i++) {
+    for (var j = 0; j < 2; j++) {
+      var left = i;
+      var right = i + j;
+      while (s[left] && s[left] === s[right]) {
+        left--;
+        right++;
+      }
+      if (right - left - 1 > max.length) {
+        max = s.substring(left + 1, right);
+      }
+    }
+  }
+  return max;
+};
