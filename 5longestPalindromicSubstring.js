b43d38cb1698aead15722b9fe9543d21b0464389
kilcmn <kilcmn3@hotmail.com>
2020-10-23 20:52:34 -0400

    added explantation 5 longest...

--- 5longestPalindromicSubstring.js
+++ 5longestPalindromicSubstring.js
@@ -64,9 +64,14 @@ longestPalindrome(str);
 
 //solution 2
 
+// Expand Around Center solution
+// T O(N^2)
+// S O(1)
 var longestPalindrome = function (s) {
   var max = '';
   for (var i = 0; i < s.length; i++) {
+    // this loop is to take into account
+    // different palindromes like 'aba' and 'abba'
     for (var j = 0; j < 2; j++) {
       var left = i;
       var right = i + j;
@@ -74,6 +79,12 @@ var longestPalindrome = function (s) {
         left--;
         right++;
       }
+      // here imagine we get into string like
+      // "sabbad", then
+      // right = 5
+      // left = 0
+      // and actual length of "abba" should be "4"
+      // 5 - 0 - 1 === 4
       if (right - left - 1 > max.length) {
         max = s.substring(left + 1, right);
       }
