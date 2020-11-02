861c214ec8479fc6cac9c126f2e898f58776c34b
kilcmn <kilcmn3@hotmail.com>
2020-11-01 23:54:55 -0500

    working on  15 3 sums..

--- 15threeSum.js
+++ 15threeSum.js
@@ -1,4 +1,5 @@
 /*
+https://leetcode.com/problems/3sum/
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
 Notice that the solution set must not contain duplicate triplets.
@@ -19,3 +20,5 @@ Input: nums = [0]
 Output: []
 
 */
+
+var threeSum = function (nums) {};
