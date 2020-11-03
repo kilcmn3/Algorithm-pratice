5d41ece4b7e743c6aa061349fe14b75758c29bce
kilcmn <kilcmn3@hotmail.com>
2020-11-02 23:15:39 -0500

    working on 19 remove Nth...

--- 19removeNthNodeFromEndofList.js
+++ 19removeNthNodeFromEndofList.js
@@ -27,3 +27,17 @@ The number of nodes in the list is sz.
 1 <= n <= sz
 
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {};
