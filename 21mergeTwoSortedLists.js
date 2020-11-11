53435a4633fb54f4ae536aab3ab1d20b02cb2426
kilcmn <kilcmn3@hotmail.com>
2020-11-11 10:25:04 -0500

    21 merged... solved

--- 21mergeTwoSortedLists.js
+++ 21mergeTwoSortedLists.js
@@ -26,4 +26,21 @@ The number of nodes in both lists is in the range [0, 50].
 Both l1 and l2 are sorted in non-decreasing order.
 */
 
-var mergeTwoLists = function (l1, l2) {};
+var mergeTwoLists = function (l1, l2) {
+  let node = new ListNode(0);
+  let copy = node;
+
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      copy.next = l1;
+      l1 = l1.next;
+    } else {
+      copy.next = l2;
+      l2 = l2.next;
+    }
+    copy = copy.next;
+  }
+  copy.next = l1 || l2;
+
+  return node.next;
+};
