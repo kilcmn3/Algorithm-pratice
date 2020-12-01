688cf62d1b0024f6259d01cac246dfca638922a0
david <kilcmn3@hotmail.com>
2020-11-30 23:54:10 -0500

    my solution to 509 Fibonacci..

--- 509FibonacciNumber.js
+++ 509FibonacciNumber.js
@@ -31,21 +31,20 @@ Note:
 */
 
 var fibanocci = function (n) {
+  if (n === 0) return 0;
+  if (n === 1 || n === 2) return 1;
 
-    if(n === 0) return 0
-    if(n === 1 || n === 2) return 1
+  let left = 0;
+  let right = 1;
+  let previous = left;
 
-    let left = 0
-    let right = 1
-    let previous = left
+  for (let i = 1; i < n; i++) {
+    previous = left;
+    left = right;
+    right = previous + right;
+  }
 
-    for(let i = 0; i < n; i++){
-      previous = left
-      left =  right;
-      right = previous + right
-    }
-
-    return right;
+  return right;
 };
 
 console.log(fibanocci(12));
