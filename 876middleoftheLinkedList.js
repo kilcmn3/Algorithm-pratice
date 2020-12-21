75fdcdd4299f8fb215409249e1723f3fb2570958
david <kilcmn3@hotmail.com>
2020-12-20 19:01:46 -0500

    876 middle of the linked List

--- 876middleoftheLinkedList.js
+++ 876middleoftheLinkedList.js
@@ -1,31 +1,13 @@
 /*
- * https://leetcode.com/problems/middle-of-the-linked-list/
+  https://leetcode.com/problems/middle-of-the-linked-list/
  */
-var middleNode = function(head) {
-      let lo = head, hi = head;
-    while (hi && hi.next) {
-        lo = lo.next;
-        console.log("this is lo:", lo)
-        hi = hi.next.next;
-        consolelog("this is hi:", hi)
-    }
-    console.log("final ans": lo)
-    return lo;
-};
 
-var head = {
-  val: 1,
-  next: {
-    val: [2,3,4,5],
-    next: {
-      val: [3,4,5],
-      next:{
-        val: [4,5],
-        next: {
-          val: [5],
-          next: null
-        }
-      }
-    }
+var middleNode = function (head) {
+  let lo = head;
+  let hi = head;
+  while (hi && hi.next) {
+    lo = lo.next;
+    hi = hi.next.next;
   }
-}
+  return lo;
+};
