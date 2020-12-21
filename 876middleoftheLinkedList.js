9fd0ba0553a5aaa6c0b7f10048aa51e2db8322e6
david <kilcmn3@hotmail.com>
2020-12-20 19:02:04 -0500

    no solution 876..

--- 876middleoftheLinkedList.js
+++ 876middleoftheLinkedList.js
@@ -2,12 +2,4 @@
   https://leetcode.com/problems/middle-of-the-linked-list/
  */
 
-var middleNode = function (head) {
-  let lo = head;
-  let hi = head;
-  while (hi && hi.next) {
-    lo = lo.next;
-    hi = hi.next.next;
-  }
-  return lo;
-};
+var middleNode = function (head) {};
